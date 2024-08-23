0178b921c0180171eef21adac149c204b158a377
Akingbogun Zakariyau <73138104+zololade@users.noreply.github.com>
2024-08-23 21:42:18 +0100

    caret

---
+++ main.js
@@ -0,0 +1,8 @@
+const titleH1 = document.querySelector(".title h1");
+
+titleH1.addEventListener("animationend", () => {
+  setTimeout(() => {
+    titleH1.style.width = "100%";
+    titleH1.style.animation = "blink-caret 0.5s step-end infinite 0s";
+  }, 3500);
+});
