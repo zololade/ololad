f2a669f789ca0c0542dfd035ae0bb0e2f581a0b5
Akingbogun Zakariyau <73138104+zololade@users.noreply.github.com>
2024-08-24 12:54:24 +0100

    Refine animation timing for welcome text
    Increase the animation duration of the welcome text to create a more visually appealing typing effect. This makes the intro more engaging and provides a smoother experience for visitors.

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const titleH1 = document.querySelector(".title h1");
 
 titleH1.addEventListener("animationend", () => {
   setTimeout(() => {
-    titleH1.style.animation = "blink-caret 0.5s step-end infinite 0s";
+    titleH1.style.animation = "blink-caret 1s step-end infinite 0s";
   }, 3500);
 });
